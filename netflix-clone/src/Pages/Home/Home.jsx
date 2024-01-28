af721be04d9c8d5a0275ccdb2e0548f3cc24c6e5
yonas-eyasu <eyasuyonas49@gmail.com>
2024-01-28 03:54:13 +0300

    row

--- netflix-clone/src/Pages/Home/Home.jsx
+++ netflix-clone/src/Pages/Home/Home.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import Header from '../../Componenets/Header/Header'
 import Footer from '../../Componenets/Footer/Footer'
 import Banner from '../../Componenets/Banner/Banner'; 
-
+import RowList from "../../Componenets/Rows/RowList/RowList"
 const Home = () => {
   return (
     <>
            <Header />
            <Banner />
+           < RowList/>
            <Footer />
            
     </>
